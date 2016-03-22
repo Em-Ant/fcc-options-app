994eb7b86a53e48d81b03d1dc3682113aaf42eb2
EmAnt <joshua999x@gmail.com>
2016-03-22 21:22:24 +0100

    update router and add redirecting to main

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -12,33 +12,40 @@ var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
 var Main = React.createClass({
 
   render: function() {
+              /*<img src="img/clementine_150.png"/>*/
+
+              /*
+              <p>Welcome,
+                <span id="display-name">{this.props.user.username}</span>!</p>
+              <Link className="menu" to="/profile">Profile</Link>
+              <p>|</p>
+              */
+
+              /*
+              <div className="container">
+                <p>You have clicked the button
+                  <span id="click-nbr">{this.props.clicks}</span>
+                  times.</p>
+                <br/>
+                <div className="btn-container">
+                  <ButtonToolbar className="text-center">
+                    <Button onClick={this.props.click} className="btn-nofloat">CLICK ME!</Button>
+                    <Button onClick={this.props.reset} className="btn-nofloat">RESET</Button>
+                  </ButtonToolbar>
+                </div>
+              </div>
+              */
     return (
       <div>
         <header>
-          <p>Welcome,
-            <span id="display-name">{this.props.user.username}</span>!</p>
-          <Link className="menu" to="/profile">Profile</Link>
-          <p>|</p>
           <a className="menu" href="/logout">Logout</a>
         </header>
 
         <div className="container">
-          <img src="img/clementine_150.png"/>
-          <br/>
-          <p className="clementine-text">Clementine.js</p>
-        </div>
 
-        <div className="container">
-          <p>You have clicked the button
-            <span id="click-nbr">{this.props.clicks}</span>
-            times.</p>
           <br/>
-          <div className="btn-container">
-            <ButtonToolbar className="text-center">
-              <Button onClick={this.props.click} className="btn-nofloat">CLICK ME!</Button>
-              <Button onClick={this.props.reset} className="btn-nofloat">RESET</Button>
-            </ButtonToolbar>
-          </div>
+          <p className="clementine-text">Main Page Test</p>
+
         </div>
       </div>
     )
