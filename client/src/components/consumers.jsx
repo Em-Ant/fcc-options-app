fa3bf2a9088aa58e4f020bfe62f5940df145f1d5
EmAnt <joshua999x@gmail.com>
2016-03-25 17:06:27 +0100

    Add Consumer create form

--- client/src/components/consumers.jsx
+++ client/src/components/consumers.jsx
@@ -41,15 +41,52 @@ var Consumers = React.createClass({
   // getInitialState: function() {
   //   return ({email: '', password: ''});
   // },
-  handleAddRouteButton: function(e) {
-    //Toggle Add Route Button
-    this.setState({
-      addRoute: !this.state.addRoute
-    });
+  handleAddConsumer: function(e) {
+
+    // This function needs validation
+    e.preventDefault();
+    var newConsumer = {};
+    newConsumer.name = this.refs.name.value;
+    newConsumer.address = this.refs.address.value;
+    newConsumer.phone = this.refs.phone.value;
+    newConsumer.sex = this.refs.sex.value;
+
+    newConsumer.needsWave = this.refs.wave.checked;
+    newConsumer.hasSeizures = this.refs.seiz.checked;
+    newConsumer.hasWeelchair = this.refs.wheel.checked;
+    newConsumer.needsTwoSeats = this.refs.twoSeats.checked;
+    newConsumer.hasMedications = this.refs.med.checked;
+    newConsumer.cannotSitNearOppositeSex = this.refs.behavior.checked;
+
+    this.refs.name.value = '';
+    this.refs.address.value = '';
+    this.refs.phone.value = '';
+    this.refs.sex.value = '';
+
+    $('input').iCheck('uncheck');
+
+
+    console.log(this.refs.wave)
+    var self = this;
+    Ajax.post('/api/consumer/', newConsumer, function(err, data) {
+      if(err) {
+        return;
+      }
+
+      Ajax.get('/api/consumer/', function(err, data){
+        if(err) {
+          // TODO
+        }
+
+        self.setState({
+          consumers: data
+        })
+      })
+    })
   },
   getInitialState: function() {
     return ({
-      "routes":[]
+      "consumers":[]
     });
   },
   componentDidMount: function() {
@@ -59,56 +96,152 @@ var Consumers = React.createClass({
       }
 
       this.setState({
-        routes: data
+        consumers: data
       })
     }.bind(this));
   },
   render: function() {
     return (
       <div className="content-wrapper">
-
-        <section className="content-header">
-          <h1>
-            Consumers
-          </h1>
-        </section>
-
         <section className="content">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="box box-primary">
+                <div className="box-header with-border">
+                  <h3 className="box-title">Consumers</h3>
+                </div>
+                <div className="box-body">
+                  <table className="table table-striped">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Sex</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Issues</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.consumers.map(function(consumer, index) {
+                        return (
+                          <tr key={index}>
+                            <td>{consumer.name}</td>
+                            <td>{consumer.sex}</td>
+                            <td>{consumer.address}</td>
+                            <td>{consumer.phone}</td>
+                            <td>
+                              {consumer.hasWeelchair ? <span className="label label-primary">Weelchair</span> : null}
+                              {consumer.hasSeizures ? <span className="label label-danger">Seizures</span> : null}
+                              {consumer.hasMedications ? <span className="label label-warning">Medications</span> : null}
+                              {consumer.needsTwoSeats ? <span className="label label-default">Two Seats</span> : null}
+                              {consumer.needsWave ? <span className="label label-info">Needs Wave</span> : null}
+                              {consumer.cannotSitNearOppositeSex ? <span className="label label-success">Behavioral Issues</span> : null}
+                            </td>
+                          </tr>
+                          );
+                        })
+                      }
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="box box-info">
+                <div className="box-header with-border">
+                  <h3 className="box-title">Add a Consumer</h3>
+                </div>
 
-          <table className="table table-striped">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Address</th>
-                <th>Phone</th>
-                <th>Issues</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.routes.map(function(consumer, index) {
-                return (
-                  <tr key={index}>
-                    <td>{consumer.name}</td>
-                    <td>{consumer.sex}</td>
-                    <td>{consumer.address}</td>
-                    <td>{consumer.phone}</td>
-                    <td>
-                      {consumer.hasWeelchair ? <span className="label label-primary">Weelchair</span> : null}
-                      {consumer.hasSeizures ? <span className="label label-danger">Seizures</span> : null}
-                      {consumer.hasMedications ? <span className="label label-warning">Medications</span> : null}
-                      {consumer.needsTwoSeats ? <span className="label label-default">Two Seats</span> : null}
-                      {consumer.needsWave ? <span className="label label-info">Needs Wave</span> : null}
-                      {consumer.cannotSitNearOppositeSex ? <span className="label label-success">Behavioral Issues</span> : null}
-                    </td>
-                  </tr>
-                  );
-                })
-              }
-            </tbody>
-          </table>
+                <form className="form-horizontal" onSubmit={this.handleAddConsumer}>
+                  <div className="box-body">
+                    <div className="form-group">
+                      <label htmlFor="c_name" className="col-sm-2 control-label">Name</label>
+                      <div className="col-sm-10">
+                        <input type="text" className="form-control" id="c_name" placeholder="Name" ref="name"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_sex" className="col-sm-2 control-label">Sex</label>
+                      <div className="col-sm-10">
+                        <select className="form-control select2" style={{'width': '100%'}}  id="c_sex" ref="sex">
+                          <option defaultValue value="male">Male</option>
+                          <option value="female">Female</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_address" className="col-sm-2 control-label">Address</label>
+                      <div className="col-sm-10">
+                        <input type="text" id="c_phone" className="form-control" id="c_address" placeholder="Address" ref="address"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_phone" className="col-sm-2 control-label">Phone</label>
+                      <div className="col-sm-10">
+                        <input type="text" className="form-control" id="c_phone" placeholder="Phone" ref="phone"/>
+                      </div>
+                    </div>
+
+
+                    <div className="form-group">
+                      <label htmlFor="c_wheel" className="col-sm-2 control-label">Weelchair</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_wheel" className="minimal" ref="wheel"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_twoseat" className="col-sm-2 control-label">Two Seats</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_twoseat" className="minimal" ref="twoSeats"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_wave" className="col-sm-2 control-label">Needs Wave</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_wave" className="minimal" ref="wave"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_behavior" className="col-sm-2 control-label">Behavioral Issues</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_behavior" className="minimal" ref="behavior"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_seiz" className="col-sm-2 control-label">Seizures</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_seiz" className="minimal" ref="seiz"/>
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label htmlFor="c_med" className="col-sm-2 control-label">Medications</label>
+                      <div className="col-sm-10">
+                        <input type="checkbox" id="c_med" className="minimal" ref="med"/>
+                      </div>
+                    </div>
+
+                  </div>
+                  <div className="box-footer">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                  </div>
+
+                </form>
+
+                </div>
+              </div>
+            </div>
+
         </section>
-
       </div>
 
     )
