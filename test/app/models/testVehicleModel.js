f3ab56c170b97cf551c9297228123c5993ab5744
Andre Alonzo <aalonzo@192.168.1.122>
2016-04-01 08:52:42 -0700

    added unit tests for us to implement

--- test/app/models/testVehicleModel.js
+++ test/app/models/testVehicleModel.js
@@ -20,4 +20,67 @@ describe('vehicleModel', function() {
     assert.equal(vehicle.maxFoldableSeats, maxFoldableSeats);
     assert.equal(vehicle.maxWheelchairs, maxWheelchairs);
   })
+
+  it('should display number of available seats', function() {
+
+    var name = "name";
+    var maxFixedSeats = 1;
+    var expectedAvailableSeats = 1;
+    var vehicle = new Vehicle({
+      name: name,
+      maxFixedSeats: maxFixedSeats
+    });
+    assert.equal(expectedAvailableSeats, vehicle.availableSeats);
+  })
+
+  it('should add up fixed and foldable seats as available seats', function() {
+
+    var name = "name";
+    var maxFixedSeats = 1;
+    var maxFoldableSeats = 1;
+    var expectedAvailableSeats = maxFixedSeats + maxFoldableSeats;
+    var vehicle = new Vehicle({
+      name: name,
+      maxFixedSeats: maxFixedSeats,
+      maxFixedSeats: maxFixedSeats
+    });
+    assert.equal(expectedAvailableSeats, vehicle.availableSeats);
+  })
+
+  it('should display number of available wheelchairs', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should decrement available seats when consumer is added', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should decrement available seats when wheelchair consumer is added', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should decrement available seats when two seat consumer is added', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should increment available seats when consumer is removed', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should increment available seats when wheelchair consumer is removed', function() {
+    //write test
+    assert(false);
+  })
+
+  it('should increment available seats when two seat consumer is removed', function() {
+    //write test
+    assert(false);
+  })
+
+
 });
