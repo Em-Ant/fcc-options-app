aabd8c66f013605c7b581fce4386c15d26a2f533
EmAnt <joshua999x@gmail.com>
2016-04-02 19:01:10 +0200

    GMaps experiments - Complex Icon

--- client/src/components/mapTest.jsx
+++ client/src/components/mapTest.jsx
@@ -15,6 +15,7 @@ var GMap = React.createClass({
   getInitialState: function () {
     return {markers : []}
   },
+  image : {},
   onMapCreated(map) {
     map.setOptions({
       disableDefaultUI: true
@@ -22,6 +23,16 @@ var GMap = React.createClass({
     var self = this;
     var geocoder = new google.maps.Geocoder();
     var address = '16820 197th Ave NW,Big Lake, MN 55309, USA';
+    this.image = {
+      url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+      // This marker is 20 pixels wide by 32 pixels high.
+      size: new google.maps.Size(20, 32),
+      // The origin for this image is (0, 0).
+      origin: new google.maps.Point(0, 0),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 32)
+    };
+
     geocoder.geocode({'address': address}, function(results, status) {
       console.log(status, results);
       var markers = self.state.markers;
@@ -48,7 +59,6 @@ var GMap = React.createClass({
   },
 
   render() {
-
     return (
       <Gmaps
         width={'800px'}
@@ -64,12 +74,13 @@ var GMap = React.createClass({
             <Marker
               lat={m.lat}
               lng={m.lng}
+              icon={this.image}
               draggable={false}
               clickable={true}
               onClick={function(){console.log('marker click')}}
             />
           )
-        })}
+        }.bind(this))}
 
       </Gmaps>
     );
