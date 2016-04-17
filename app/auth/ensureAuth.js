f705dd07be24228eb577b8d94ba489703e7b29cf
Andre Alonzo <aalonzo@192.168.1.122>
2016-04-17 12:28:44 -0700

    fixed disable_auth

--- app/auth/ensureAuth.js
+++ app/auth/ensureAuth.js
@@ -5,7 +5,7 @@
 
 
 function isLoggedIn(req, res, next) {
-  if(process.env.DISABLE_AUTH==true){
+  if(process.env.DISABLE_AUTH=="true"){
       return next();
   }
   if (req.isAuthenticated()) {
@@ -19,7 +19,7 @@ function isLoggedIn(req, res, next) {
 
 function hasRole(role) {
   return function(req, res, next) {
-    if(process.env.DISABLE_AUTH==true){
+    if(process.env.DISABLE_AUTH=="true"){
         return next();
     }
     if (req.isAuthenticated()) {
