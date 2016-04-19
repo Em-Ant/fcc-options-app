5be2b963d3ef4794a2874c2df30dae9a29bcadaf
EmAnt <joshua999x@gmail.com>
2016-04-19 22:48:37 +0200

    printing test

--- client/src/components/directions/directions.jsx
+++ client/src/components/directions/directions.jsx
@@ -4,8 +4,23 @@ var React = require('react');
 var connect = require('react-redux').connect;
 var actions = require('../../actions/mapActions');
 var DirectionsBody = require('./directionsBody.jsx');
+var printStyle = require('raw!./printStyle.txt')
 
 var Directions = React.createClass({
+  print: function() {
+    console.log('print', printStyle)
+    var w=window.open();
+    w.document.write('<!DOCTYPE html><html><head>');
+    //w.document.write('<link rel="stylesheet" href="static/adminLTE/bootstrap/css/bootstrap.min.css" media="print"/>');
+    w.document.write('<style type="text/css">');
+    w.document.write(printStyle);
+    w.document.write('</style></head><body>');
+    w.document.write(document.getElementById('print').innerHTML);
+    w.document.write('</body></html>')
+    w.print();
+    //w.close();
+
+  },
   render: function() {
     return (
       <div className="nav-tabs-custom">
@@ -13,7 +28,7 @@ var Directions = React.createClass({
               <li className=""><a href="#" className="text-muted" onClick={this.props.onBackClick}><i className="fa fa-chevron-left cust-btn"></i></a></li>
               <li className="active"><a href="#tab_1" data-toggle="tab" aria-expanded="false">AM Directions</a></li>
               <li className=""><a href="#tab_2" data-toggle="tab" aria-expanded="true">PM Directions</a></li>
-              <li className="pull-right"><a href="#"><i className="fa fa-print cust-btn"></i></a></li>
+              <li className="pull-right"><a href="#" onClick={this.print}><i className="fa fa-print cust-btn"></i></a></li>
             </ul>
             <div className="tab-content">
               <div className="tab-pane active" id="tab_1">
