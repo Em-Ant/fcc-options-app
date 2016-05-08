43877b913fe819972a8de2eefe8719e623fdb8e8
EmAnt <joshua999x@gmail.com>
2016-05-08 16:35:47 +0200

    filter components in consumers page

--- client/src/components/consumersPage/consumersPageComponent.jsx
+++ client/src/components/consumersPage/consumersPageComponent.jsx
@@ -60,33 +60,62 @@ var Consumers = React.createClass({
                 <div className="box-header with-border">
                   <h3 className="box-title">Consumers</h3>
                   <form className="box-head-form">
-                    <label>
-                      <input id="namefilter"type="text"
-                        onChange={this.props.setFilterString}
-                        value={this.props.textFilter}
-                        ref="filter" placeholder="filter">
-                      </input>
-                    </label>
-                    <label className="radio-inline">
-                      <input
-                        type="radio"
-                        name="filter_type"
-                        id="c_name_radio"
-                        value="name"
-                        onChange={this.props.setFilterType}
-                        checked={this.props.filterType === "name"}/>
-                        &nbsp;name
-                    </label>
-                    <label className="radio-inline">
-                      <input
-                        type="radio"
-                        name="filter_type"
-                        id="c_address_radio"
-                        value="address"
-                        onChange={this.props.setFilterType}
-                        cheched={this.props.filterType === "address"}/>
-                        &nbsp;address
-                    </label>
+                    <div className="form-group">
+                      <label>
+                        <input
+                          type="radio"
+                          name="filter_type"
+                          id="c_name_radio"
+                          value="name"
+                          onChange={this.props.setFilterType}
+                          checked={this.props.filterType === "name"}/>
+                          &nbsp;name
+                      </label>
+                      <label>
+                        <input
+                          type="radio"
+                          name="filter_type"
+                          id="c_address_radio"
+                          value="address"
+                          onChange={this.props.setFilterType}
+                          cheched={this.props.filterType === "address"}/>
+                          &nbsp;address
+                      </label>
+                      <label>
+                        <input id="namefilter"type="text"
+                          onChange={this.props.setFilterString}
+                          value={this.props.textFilter}
+                          ref="filter" placeholder="filter">
+                        </input>
+                      </label>
+                      <label>
+                        <input type="checkbox"/> M
+                      </label>
+                      <label>
+                        <input type="checkbox"/> F
+                      </label>
+                      <label>
+                        <input type="checkbox"/> <i className="fa fa-wheelchair"></i>
+                      </label>
+                      <label>
+                        <input type="checkbox"/> <i className="fa fa-medkit"></i>
+                      </label>
+                      <label>
+                        <input type="checkbox"/> SZ
+                      </label>
+                      <label>
+                        <input type="checkbox"/> 2S
+                      </label>
+                      <label>
+                        <input type="checkbox"/> <i className="fa fa-phone"></i>
+                      </label>
+                      <label>
+                        <input type="checkbox"/> BI
+                      </label>
+                      <label>
+                        <input type="checkbox"/> No Needs
+                      </label>
+                    </div>
                   </form>
                   <span className="pull-right">
                     <button className="btn btn-success" onClick={this.props.setAddMode}>
